9c3dc1d38d6f66e48649d5dc483fecfb606453cc
Robert Ball <robert.ball.1989@gmail.com>
2017-05-04 14:47:24 +0100

    Properly moved canvas functionality.

--- client/components/CanvasComponent.jsx
+++ client/components/CanvasComponent.jsx
@@ -1,8 +1,19 @@
-// class CanvasComponent
+import React from 'react';
+import io from 'socket.io-client'
 
+export default class CanvasComponent extends React.Component {
+componentDidMount(){
+  this.updateCanvas()
+}
 
+updateCanvas(){
+  const context = 
+  context.fillRect(0,0, 100, 100)
+}
 
-
-
-
-
+render(){
+  return (
+    <canvas id='main-canvas' width='600' height='500'/>
+    )
+}
+}
--- client/containers/CanvasContainer.jsx
+++ client/containers/CanvasContainer.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
 import io from 'socket.io-client'
+import CanvasComponent from '../components/CanvasComponent'
 
 export default class CanvasContainer extends React.Component {
-
-  componentDidMount(){
-    this.updateCanvas()
+  constructor(props){
+    super(props)
   }
-
-  updateCanvas(){
-    const context = 
-    context.fillRect(0,0, 100, 100)
-  }
-
-  render(){
-    return (
-      <canvas id='main-canvas' width='600' height='500'/>
-      )
-  }
-  // constructor(props){
-  //   super(props)
-
-
-
-  // }
 
 
   // makeRectangle(){
@@ -30,16 +13,16 @@ export default class CanvasContainer extends React.Component {
   //   context.fillRect(10,10,50,50)
   // }
 
-  // // canvas.onmousemove = function(event){
-  // //   drawCircle(event.x, event.y)
-  // // }
-
-  // render(){
-  //   return(
-  //     <div id="canvas-container">
-  //       <canvas id='main-canvas' width='600' height='500'></canvas>
-  //     </div>
-  //   )
+  // canvas.onmousemove = function(event){
+  //   drawCircle(event.x, event.y)
   // }
+
+  render(){
+    return(
+      <div id="canvas-container">
+        <CanvasComponent/>
+      </div>
+    )
+  }
 
 }
